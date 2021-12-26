f37a854fb34a768c8dd742d647e90ecc33b02bd1
yurtemre7 <emre.yurtseven.1@campus.tu-berlin.de>
2021-12-26 15:32:20 +0100

    no more darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,9 @@
 import '../styles/globals.css'
-import { useMemo } from 'react';
 import type { AppProps } from 'next/app'
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode],
-  );
-  
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Component {...pageProps} />
   )
-
 }
 export default MyApp
