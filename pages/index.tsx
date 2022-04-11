64b3f6bd1c9d8744dc41c3c4aa4ee727a493a75d
yurtemre7 <emre.yurtseven.1@campus.tu-berlin.de>
2022-04-11 10:27:33 +0200

    4. semester baby

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
           <Greeting user={user} />
 
           <p className={styles.description}>
-            Emre ist ein Informatik Student im <code className={styles.code}>3.</code> Semester
+            Emre ist ein Informatik Student im <code className={styles.code}>4.</code> Semester
             und arbeitet in der Cross-Platform App-Entwicklung mit <code className={styles.code}><a target="_blank" rel="noopener noreferrer" href='https://flutter.dev/'>Flutter</a></code>
             am <Button href="https://www.appmelder.de/" target="_blank" rel="noopener noreferrer" variant="text">Appmelder</Button>.
           </p>
